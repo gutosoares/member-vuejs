e5ad88e80e2a94e4aed016cf3a1ae2eac3128f37
Augusto Soares <gutosoares@outlook.com>
2017-04-23 00:47:51 -0300

    Config routes for the AddMember component

--- src/main.js
+++ src/main.js
@@ -4,25 +4,35 @@ import VueResource from 'vue-resource'
 
 import App from './App.vue'
 import Members from './components/Members.vue'
+import AddMember from './components/AddMember.vue'
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
 
 const routes = [
-  {path: '/', component: Members}
+	{path: '/', component: Members},
+	{path: '/add', component: AddMember}
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: __dirname,
-  routes
+	mode: 'history',
+	base: __dirname,
+	routes
 })
 
 new Vue({
-  router,
-  template: `
-    <div id="app">
-      <router-view></router-view>
-    </div>
-  `
+	router,
+	template: `
+		<div id="app">
+			<nav>
+				<ul>
+					<router-link to="/"><button>Members</button></router-link>
+					<router-link to="/add"><button>Add Member</button></router-link>
+				</ul>
+			</nav>
+			<main>
+				<router-view></router-view>
+			</main>	
+		</div>
+	`
 }).$mount('#app')
