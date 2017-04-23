e6819878656456297e9083ab282679984e574ede
Augusto Soares <gutosoares@outlook.com>
2017-04-23 00:10:38 -0300

    List all members on db.json file

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -1,14 +1,52 @@
 <template>
     <div id="members">
-    <h1>Members</h1>
+        <h1>Members</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Office</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="member in members">
+                    <td>{{ member.name }}</td>
+                    <td>{{ member.email }}</td>
+                    <td>{{ member.office }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'members'
+    name: 'members',
+
+    data() {
+        return {
+            members: []
+        }
+    },
+
+    methods: {
+        fetchMembers() {
+            var self = this
+            self.$http.get('http://localhost:3000/customers').then(response => {
+                self.members = response.data
+            })
+        }
+    },
+
+    mounted() {
+        this.fetchMembers()
+    }
 }
 </script>
 
 <style>
+table, th, td {
+    border: 1px solid #000;
+}
 </style>
