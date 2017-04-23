3b91da5efb152022068548a0a737ac10d937cdb7
Augusto Soares <gutosoares@outlook.com>
2017-04-22 23:27:56 -0300

    Add component Members

---
+++ src/components/Members.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="members">
+    <h1>Members</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'members'
+}
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
 import App from './App.vue'
+import Members from './components/Members.vue'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+const routes = [
+  {path: '/', component: Members}
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes
+})
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  router,
+  template: `
+    <div id="app">
+      <router-view></router-view>
+    </div>
+  `
+}).$mount('#app')
