810d7c3419551879502768ea9db2a7d6045a0906
Augusto Soares <gutosoares@outlook.com>
2017-04-24 12:05:24 -0300

    Edit change buttons style

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -6,8 +6,11 @@
                 <input type="text" id="search" v-model="filter">
                 <label for="search">Search Member by Office</label>
             </div>
-            <div class="col s6">
-                <h1>Members</h1>
+            <div class="col s6 change-view right-align">
+                <span>Visualizar: </span>
+                <a href="#" class="btn btn-item btn-disable">Freelas</a>
+                <a href="#" class="btn btn-item">Usuários</a>
+                <router-link to="/add"><a class="add-member waves-effect waves-light btn-large">Add Member</a></router-link>
             </div>
         </div>
         <div class="row">
@@ -73,15 +76,19 @@ export default {
 <style>
 /* Search Input */
 .input-field .material-icons {
-    margin-top: 21px;
-    margin-left: 11px;
+    margin-top: 12px;
+    margin-left: 42px;
     font-size: 22px;
-    color: #ccc;
+    color: rgba(168, 168, 168, 0.25);
 }
 
 .input-field label {
     font-size: 13px;
     color: rgba(101, 157, 184, 0.76);
+}
+
+.input-field .prefix ~ label {
+    margin-left: 5.4rem;
 }
 
 .input-field .prefix.active {
@@ -90,7 +97,8 @@ export default {
 
 .input-field input[type=text] {
     margin-top: -3px;
-    border-bottom: 1px solid #ccc;
+    padding-left: 35px;
+    border-bottom: 1px solid rgba(168, 168, 168, 0.25);
     color: #a8a8a8;
 }
 
@@ -102,7 +110,38 @@ export default {
     color: #86c06e;
 }
 
-table, th, td {
-    border: 1px solid #000;
+/* Change options */
+.change-view {
+    margin-top: 20px;
+}
+.change-view span {
+    color: rgba(101, 157, 184, 0.76);
+    margin-right: 20px;
+}
+
+.change-view a:nth-child(3) {
+    margin-right: 20px;
+}
+
+.change-view .btn-item {
+    background-color: #eff2f3;
+    border: 1px solid rgba(168, 168, 168, 0.25);
+    color: rgba(101, 157, 184, 0.76);
+    box-shadow: none;
+    font-size: 13px;
+}
+
+.change-view a:nth-child(2) {
+    margin-right: -6px;
+}
+
+.btn-disable {
+    background-color: #fafafa !important;
+    color: rgba(168, 168, 168, 0.6) !important;
+}
+
+/* Button Add Member */
+.add-member {
+    background-color: #86c06e;
 }
 </style>
